6855a70c4490a004a601e55a85cdb79667a406b7
Ulises Santana <ulises.santana@netexlearning.net>
2020-06-13 23:32:30 +0100

    :rocket: change from yarn to npm for making github packages work

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
-import {ToggleDashboard} from "@fefo/toggl-dashboard";
-import {Menu, TodoSection} from "./components";
-import {BrowserStore} from "@fefo/toggl-dashboard/dist";
-import {Section} from "./types";
+import { ToggleDashboard } from "@fefo-app/toggl-dashboard";
+import { Menu, TodoSection } from "./components";
+import { BrowserStore } from "@fefo-app/toggl-dashboard/dist";
+import { Section } from "./types";
 
 function App() {
-    const storedActiveSection = BrowserStore.get('fefo:active-section');
-    const [activeSection, setSection] = useState(storedActiveSection as Section || Section.ToggleDashboard)
-    const storeSection = (section: Section) => {
-        BrowserStore.set('fefo:active-section', section)
-        setSection(section)
-    }
+  const storedActiveSection = BrowserStore.get("fefo:active-section");
+  const [activeSection, setSection] = useState(
+    (storedActiveSection as Section) || Section.ToggleDashboard
+  );
+  const storeSection = (section: Section) => {
+    BrowserStore.set("fefo:active-section", section);
+    setSection(section);
+  };
 
-    return (
-        <div className="App">
-            <Menu activeSection={activeSection} onSectionChange={storeSection} />
-            <main>
-                {activeSection === Section.TodoApp && <TodoSection/>}
-                {activeSection === Section.ToggleDashboard && <ToggleDashboard/>}
-            </main>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Menu activeSection={activeSection} onSectionChange={storeSection} />
+      <main>
+        {activeSection === Section.TodoApp && <TodoSection />}
+        {activeSection === Section.ToggleDashboard && <ToggleDashboard />}
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/components/TodoSection.tsx
+++ src/components/TodoSection.tsx
@@ -1,58 +1,56 @@
-import React, {useState} from "react";
-import {AddTodo, TodoApp} from "@fefo/todo-app";
-import {BrowserStore} from "@fefo/toggl-dashboard/dist";
-import {TrashButton} from "@fefo/toggl-dashboard/dist";
+import React, { useState } from "react";
+import { AddTodo, TodoApp } from "@fefo-app/todo-app";
+import { BrowserStore } from "@fefo-app/toggl-dashboard/dist";
+import { TrashButton } from "@fefo-app/toggl-dashboard/dist";
 import styled from "styled-components";
 
-const NAMESPACE = 'TodoSection:todo-lists';
+const NAMESPACE = "TodoSection:todo-lists";
 
 const StyleWrapper = styled.section`
   padding: 16px;
-  
+
   ul {
     padding: 0;
     list-style: none;
-    
+
     li {
       padding: 0;
       position: relative;
-      
+
       & > button {
         position: absolute;
         right: 0;
-        top: 36px
+        top: 36px;
       }
     }
   }
-
-`
+`;
 
 export function TodoSection() {
-    const storedTodoLists = BrowserStore.get(NAMESPACE);
-    const [todoLists, setList] = useState<string[]>(storedTodoLists || []);
-    const addTodoList = (todoList: string) => {
-        const newState = todoLists.concat(todoList)
-        BrowserStore.set(NAMESPACE, newState)
-        setList(newState)
-    }
-    const removeTodoList = (todoList: string) => {
-        const newState = todoLists.filter(t => t !== todoList)
-        BrowserStore.set(NAMESPACE, newState)
-        setList(newState)
-    }
+  const storedTodoLists = BrowserStore.get(NAMESPACE);
+  const [todoLists, setList] = useState<string[]>(storedTodoLists || []);
+  const addTodoList = (todoList: string) => {
+    const newState = todoLists.concat(todoList);
+    BrowserStore.set(NAMESPACE, newState);
+    setList(newState);
+  };
+  const removeTodoList = (todoList: string) => {
+    const newState = todoLists.filter((t) => t !== todoList);
+    BrowserStore.set(NAMESPACE, newState);
+    setList(newState);
+  };
 
-
-    return (
-        <StyleWrapper>
-            <AddTodo placeholder="Añade una nueva lista" addTodo={addTodoList}/>
-            <ul>
-                {todoLists.map(todoList =>
-                    <li key={todoList}>
-                        <TrashButton onClick={() => removeTodoList(todoList)} size={24}/>
-                        <TodoApp title={todoList}/>
-                    </li>)}
-            </ul>
-
-        </StyleWrapper>
-    )
+  return (
+    <StyleWrapper>
+      <AddTodo placeholder="Añade una nueva lista" addTodo={addTodoList} />
+      <ul>
+        {todoLists.map((todoList) => (
+          <li key={todoList}>
+            <TrashButton onClick={() => removeTodoList(todoList)} size={24} />
+            <TodoApp title={todoList} />
+          </li>
+        ))}
+      </ul>
+    </StyleWrapper>
+  );
 }
